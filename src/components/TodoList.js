ed12f98daf71131dab6be2a40e5c52828c7e6243
IselDrew <andryshonochek0604@gmail.com>
2019-07-11 14:30:59 +0300

    Add svg icon to remove-button

--- src/components/TodoList.js
+++ src/components/TodoList.js
@@ -4,9 +4,15 @@ function TodoList(props) {
     return (
         <div className='todo-item'>
             <li>{props.item}</li>
-            <div onClick={props.removeTask.bind(props, props.item)}>
-                <svg width='15' height='15'>
-                    <rect width='15' height='15' fill='red'/>
+            <div className='remove-button' onClick={props.removeTask.bind(props, props.item)}>
+                <svg viewBox='0 0 375 375' width='15' height='15' xmlns='http://www.w3.org/2000/svg'>
+                    <g>
+                        <polygon 
+                            id='svg_1'  
+                            fill='#FF3501' 
+                            points='378.303,28.285 350.018,0 189.151,160.867 28.285,0 0,28.285 160.867,189.151 0,350.018   28.285,378.302 189.151,217.436 350.018,378.302 378.303,350.018 217.436,189.151 '
+                        />
+                    </g>
                 </svg>
             </div>
         </div> 
