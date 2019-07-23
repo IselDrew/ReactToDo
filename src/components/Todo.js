c4387479404b163fc118b772a7784a6968ce27d3
IselDrew <andryshonochek0604@gmail.com>
2019-07-23 14:26:56 +0300

    Add edit feature via div and input changing

--- src/components/Todo.js
+++ src/components/Todo.js
@@ -4,6 +4,10 @@ import EditIcon from './EditIcon'
 import SaveIcon from './SaveIcon'
 
 class Todo extends Component {
+    // constructor(props) {
+    //     super(props);
+    // }
+
     render() {
         return (
             <div className='todo-item'>
@@ -13,12 +17,27 @@ class Todo extends Component {
                     checked={this.props.item.completed}
                 />
 
-                <div 
+                {this.props.item.isEdit ? 
+                    <input 
+                        type='text' 
+                        defaultValue={this.props.item.text}
+                        onBlur={(event) => {this.props.saveChanges(this.props.item.id, event)}}
+                    /> :
+                    <div> 
+                        <p className={this.props.item.completed ? 'line-through' : ''}> 
+                            {this.props.item.text} 
+                        </p> 
+                    </div> 
+                }
+
+                {/* <div 
                     contentEditable={this.props.item.isEdit} 
                     onBlur={(event) => {this.props.saveChanges(this.props.item.id, event)}}
                 >
-                    <p className={this.props.item.completed ? 'line-through' : ''}>{this.props.item.text}</p>
-                </div>
+                    <p className={this.props.item.completed ? 'line-through' : ''}>
+                        {this.props.item.text}
+                    </p>
+                </div> */}
 
                 <div onClick={() => {this.props.editTask(this.props.item.id)}}>
                     {this.props.item.isEdit ? <SaveIcon /> : <EditIcon />}
@@ -31,32 +50,5 @@ class Todo extends Component {
         )
     }
 }
-
-// function TodoList(props) {
-//     return (
-//         <div className='todo-item'>
-//             <input 
-//                 type='checkbox'
-//                 onChange={() => {props.strikeThrough(props.item.id)}}
-//                 checked={props.item.completed}
-//             />
-
-//             <div 
-//                 contentEditable={props.item.isEdit} 
-//                 onBlur={(event) => {props.saveChanges(props.item.id, event)}}
-//             >
-//                 <p className={props.item.completed ? 'line-through' : ''}>{props.item.text}</p>
-//             </div>
-
-//             <div onClick={() => {props.editTask(props.item.id)}}>
-//                 {props.item.isEdit ? <SaveIcon /> : <EditIcon />}
-//             </div>
-
-//             <div onClick={() => props.removeTask(props.item)}>
-//                 <RemoveIcon />
-//             </div>
-//         </div> 
-//     )
-// }
 
 export default Todo
