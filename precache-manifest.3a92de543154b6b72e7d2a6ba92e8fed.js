840c87034fe5812d616e2532f6f3542fd5d2f525
Andrii <andryshonochek0604@gmail.com>
2019-09-26 16:37:40 +0300

    Updates

---
+++ precache-manifest.3a92de543154b6b72e7d2a6ba92e8fed.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8aed828ff46e3e259936dde2b634d48e",
+    "url": "/React-ToDo/index.html"
+  },
+  {
+    "revision": "89324c6d5279cfcaaa8a",
+    "url": "/React-ToDo/static/css/main.deb77c41.chunk.css"
+  },
+  {
+    "revision": "48d5a02bd3d96a584de5",
+    "url": "/React-ToDo/static/js/2.087e4584.chunk.js"
+  },
+  {
+    "revision": "89324c6d5279cfcaaa8a",
+    "url": "/React-ToDo/static/js/main.1182f9ea.chunk.js"
+  },
+  {
+    "revision": "de23fa44dfcb11340406",
+    "url": "/React-ToDo/static/js/runtime~main.191123ef.js"
+  }
+]);
